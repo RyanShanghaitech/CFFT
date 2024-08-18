71da1a9713c861643ece3d8e77fdcb85ec4ff19e
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-08-18 12:32:20 +0800

    make kspace accord with analytical definition

--- cfft/Function.py
+++ cfft/Function.py
@@ -1,4 +1,7 @@
+from numpy import *
 from numpy.fft import *
 
-def cft(im): return fftshift(fftn(fftshift(im)))
-def icft(ks): return fftshift(ifftn(fftshift(ks)))
+def cft(im:ndarray) -> ndarray:
+    return fftshift(fftn(fftshift(im)))/asarray(im.shape).prod()
+def icft(ks:ndarray) -> ndarray:
+    return fftshift(ifftn(fftshift(ks)))*asarray(ks.shape).prod()
