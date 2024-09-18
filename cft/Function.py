b5be35fc048beec7f7622f30c4d2947bd6e76a8b
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-09-18 11:34:03 +0800

    remove unessential fftshift()

--- cft/Function.py
+++ cft/Function.py
@@ -2,6 +2,6 @@ from numpy import *
 from numpy.fft import *
 
 def fft(im:ndarray) -> ndarray:
-    return fftshift(fftn(fftshift(im)))/asarray(im.shape).prod()
+    return fftshift(fftn(im))/asarray(im.shape).prod()
 def ift(ks:ndarray) -> ndarray:
-    return fftshift(ifftn(fftshift(ks)))*asarray(ks.shape).prod()
+    return ifftn(ifftshift(ks))*asarray(ks.shape).prod()
--- example.py
+++ example.py
@@ -7,36 +7,47 @@ sizIm = 128
 # create phatom
 im = transform.resize(data.shepp_logan_phantom(), (sizIm, sizIm))
 
-# generate centered kspace
+# generate kspace using fft
+ksFFT = fft.fftn(im)
+imRecoFFT = fft.ifftn(ksFFT)
+
+# generate centralized kspace
 ksCFT = cft.fft(im)
 imRecoCFT = cft.ift(ksCFT)
 
-# generate raw kspace using a single fftshift
-ksFFT = fft.fftshift(fft.fftn(im))
-imRecoFFT = fft.ifftn(ksFFT)
-
-print(f"mean err = {mean(abs(ksCFT-ksFFT).flatten())}")
-print(f"mean err = {mean(abs(imRecoCFT-imRecoFFT).flatten())}")
-
 # compare
 figure()
-subplot(231)
-imshow(abs(im), cmap='gray')
-title('img')
-subplot(232)
-imshow(abs(ksCFT), cmap='gray', norm="log")
-title('img.CFT.abs')
-subplot(233)
-imshow(real(imRecoCFT), cmap='gray')
-title('img.CFT.CIFT.real')
-subplot(234)
-imshow(abs(im), cmap='gray')
-title('img')
-subplot(235)
-imshow(abs(ksFFT), cmap='gray', norm="log")
-title('img.FFT.SHIFT.abs')
-subplot(236)
-imshow(real(imRecoFFT), cmap='gray')
-title('img.FFT.SHIFT.IFFT.real')
+
+subplot(2,5,1)
+imshow(im, cmap='gray')
+title('img'); colorbar()
+subplot(2,5,2)
+imshow(abs(ksFFT), norm="log")
+title('img.FFT.abs'); colorbar()
+subplot(2,5,3)
+imshow(angle(ksFFT), cmap="hsv", vmin=-pi, vmax=pi)
+title('img.FFT.ang'); colorbar()
+subplot(2,5,4)
+imshow(imRecoFFT.real, cmap='gray')
+title('imRecoFFT.real'); colorbar()
+subplot(2,5,5)
+imshow(imRecoFFT.imag, cmap='gray')
+title('imRecoFFT.imag'); colorbar()
+
+subplot(2,5,6)
+imshow(im, cmap='gray')
+title('img'); colorbar()
+subplot(2,5,7)
+imshow(abs(ksCFT), norm="log")
+title('img.CFT.abs'); colorbar()
+subplot(2,5,8)
+imshow(angle(ksCFT), cmap="hsv", vmin=-pi, vmax=pi)
+title('img.CFT.ang'); colorbar()
+subplot(2,5,9)
+imshow(imRecoCFT.real, cmap='gray')
+title('imRecoCFT.real'); colorbar()
+subplot(2,5,10)
+imshow(imRecoCFT.imag, cmap='gray')
+title('imRecoCFT.imag'); colorbar()
 
 show()
