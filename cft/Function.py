f0297dccdaf9023fef72394ba00ed34111d3fd81
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-09-18 18:52:21 +0800

    Add fftshift() back to make FFT consist with DFT

--- cft/Function.py
+++ cft/Function.py
@@ -2,6 +2,6 @@ from numpy import *
 from numpy.fft import *
 
 def fft(im:ndarray) -> ndarray:
-    return fftshift(fftn(im))/asarray(im.shape).prod()
+    return fftshift(fftn(fftshift(im)))/asarray(im.shape).prod() # both fftshift() here is neccessary to make FFT consist with DFT
 def ift(ks:ndarray) -> ndarray:
-    return ifftn(ifftshift(ks))*asarray(ks.shape).prod()
+    return fftshift(ifftn(ifftshift(ks)))*asarray(ks.shape).prod()
