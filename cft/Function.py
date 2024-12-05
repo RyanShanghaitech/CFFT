628853a0351362c4d49f9de6837be794dd096147
Rui Luo <luorui2023@shanghaitech.edu.cn>
2024-12-06 00:29:50 +0800

    fix bug in odd point fft

--- cft/Function.py
+++ cft/Function.py
@@ -1,8 +1,14 @@
 from numpy import *
 from numpy.fft import *
 
-def cft(im:ndarray, axes:tuple|None=None) -> ndarray:
-    return fftshift(fftn(fftshift(im, axes=axes), axes=axes), axes=axes) # both fftshift() here is neccessary to make FFT consist with DFT
+def cft(x:ndarray, axes:tuple|None=None) -> ndarray:
+    x = ifftshift(x, axes=axes)
+    x = fftn(x, axes=axes)
+    x = fftshift(x, axes=axes)
+    return x # both ifftshift() and fftshift() here is neccessary to make FFT consist with DFT
 
-def ift(ks:ndarray, axes:tuple|None=None) -> ndarray:
-    return fftshift(ifftn(fftshift(ks, axes=axes), axes=axes), axes=axes) # both fftshift() here is neccessary to make FFT consist with DFT
+def ift(x:ndarray, axes:tuple|None=None) -> ndarray:
+    x = ifftshift(x, axes=axes)
+    x = ifftn(x, axes=axes)
+    x = fftshift(x, axes=axes)
+    return x # both ifftshift() and fftshift() here is neccessary to make FFT consist with DFT
